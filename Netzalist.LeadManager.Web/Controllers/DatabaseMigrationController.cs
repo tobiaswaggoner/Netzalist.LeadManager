9a5552896674cc1c29d7a59e9ab6d2c204cd0c0b
Tobias Waggoner <tobias.waggoner@gmail.com>
2013-10-13 19:21:30 +0200

    added databasemigration feature

---
+++ Netzalist.LeadManager.Web/Controllers/DatabaseMigrationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Netzalist.LeadManager.Web.Models;
+
+namespace Netzalist.LeadManager.Web.Controllers
+{
+    public class DatabaseMigrationController : Controller
+    {
+        //
+        // GET: /DatabaseMigration/
+
+        public ActionResult Index()
+        {
+            var model = PopulateModel();
+            return View(model);
+        }
+
+        private static DatabaseMigrationModel PopulateModel()
+        {
+            var migrator = new DbMigrator(new Migrations.Configuration());
+            var model = new DatabaseMigrationModel {PendingMigrations = new List<string>(), AllMigrations = new List<string>()};
+
+            foreach (var nxtChange in migrator.GetPendingMigrations())
+                model.PendingMigrations.Add(nxtChange);
+
+            foreach (var nxtChange in migrator.GetDatabaseMigrations())
+                model.AllMigrations.Add(nxtChange);
+            return model;
+        }
+
+        public void UpdateNow()
+        {
+            var migrator = new DbMigrator(new Migrations.Configuration());
+            migrator.Update();
+            RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Netzalist.LeadManager.Web/Models/DatabaseMigrationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Netzalist.LeadManager.Web.Models
+{
+    public class DatabaseMigrationModel
+    {
+        public List<String> AllMigrations; 
+        public List<String> PendingMigrations;
+    }
+}
