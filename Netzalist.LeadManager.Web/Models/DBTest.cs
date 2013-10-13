c42af8a7f330bbf46379b2953e1050f738f61cb5
Tobias Waggoner <tobias.waggoner@gmail.com>
2013-10-13 16:34:20 +0200

    Some Tests

---
+++ Netzalist.LeadManager.Web/Models/DBTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Netzalist.LeadManager.Web.Models
+{
+    public class DBTest : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+    }
+}
---
+++ Netzalist.LeadManager.Web/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Netzalist.LeadManager.Web.Models
+{
+    public class Test
+    {
+        [Key]
+        public int TestPK { get; set; }
+        public String Name { get; set; }
+    }
+}
